e36de2c0ce7dbcb03f4664c363c455874fff25c8
claudioaricci <claudioaricci@gmail.com>
2024-05-12 15:22:26 -0300

    feat: Configuração inicial

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css'
+import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/Footer';
+import Login from './components/Login/Login';
+
+const App = () => {
+  return <div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+      </BrowserRouter>
+    </div>;
+};
+
+export default App;
